b7dac640d3497e90f8775ab2e16728f244717929
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2021-11-16 22:55:20 -0500

    Fixed gradients

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
    <div>
-      <div id="main" class="bg-gradient-to-r from-orange-200 via-orange-600 to-blue-800 p-10 w-full h-full">
+      <div id="main" class="block bg-gradient-to-l from-red-200 via-red-600 to-blue-800 p-10 w-50 h-50">
          <NavBar/>
-         <div id="collage" class="flex flex-col justify-center items-center m-10">
+         <div id="collage" class="flex flex-col justify-center items-center">
          <DynamicImage />
          </div>
       </div>
