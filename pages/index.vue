63ebc4e90c9d0d863a8845662b08a3b959a88cc3
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2021-11-16 22:48:04 -0500

    Minor fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
    <div>
-      <div id="main" class="bg-gradient-to-t from-orange-200 to-blue-400 m-10 w-full h-full">
+      <div id="main" class="bg-gradient-to-r from-orange-200 via-orange-600 to-blue-800 p-10 w-full h-full">
          <NavBar/>
          <div id="collage" class="flex flex-col justify-center items-center m-10">
          <DynamicImage />
